0fc943cfbee97c55665b785e49a0178857054e64
Srevin Saju <srevinsaju@sugarlabs.org>
2021-06-03 18:32:11 +0300

    style: lint with black

--- lyrixd.py
+++ lyrixd.py
@@ -1,14 +1,12 @@
-
-
-
 import dbus
-import time 
+import time
 import requests
 
 
 session_bus = dbus.SessionBus()
-elisa_bus = session_bus.get_object("org.mpris.MediaPlayer2.elisa",
-                                     "/org/mpris/MediaPlayer2")
+elisa_bus = session_bus.get_object(
+    "org.mpris.MediaPlayer2.elisa", "/org/mpris/MediaPlayer2"
+)
 
 
 last_song = ""
@@ -21,15 +19,16 @@ def get_song_and_post():
     global last_song
     global was_playing
 
-    spotify_properties = dbus.Interface(elisa_bus,
-                                        "org.freedesktop.DBus.Properties")
-    playback_status = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "PlaybackStatus")
+    spotify_properties = dbus.Interface(elisa_bus, "org.freedesktop.DBus.Properties")
+    playback_status = spotify_properties.Get(
+        "org.mpris.MediaPlayer2.Player", "PlaybackStatus"
+    )
 
     metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
 
-    current_song = metadata['xesam:title']
+    current_song = metadata["xesam:title"]
     current_artist = metadata["xesam:artist"][0]
-    
+
     if was_playing and playback_status != "Playing":
         last_song = None
         last_artist = None
@@ -37,17 +36,17 @@ def get_song_and_post():
         print(f"Song play has stopped")
         requests.post("http://127.0.0.1:5555/api/currentsong/1265317047", json={})
 
-    if playback_status == "Playing" and (current_artist != last_artist or current_song != last_song):
+    if playback_status == "Playing" and (
+        current_artist != last_artist or current_song != last_song
+    ):
         was_playing = True
         last_song = current_song
         last_artist = current_artist
         print(f"Currently playing {last_song} by {last_artist}.")
-        requests.post("http://127.0.0.1:5555/api/currentsong/1265317047", json={
-            "song": last_song,
-            "artist": last_artist
-        })
-
-    
+        requests.post(
+            "http://127.0.0.1:5555/api/currentsong/1265317047",
+            json={"song": last_song, "artist": last_artist},
+        )
 
 
 def main():
