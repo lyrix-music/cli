e78e1e6a217b200fe06ad14d0c272dbca58cd161
Srevin Saju <srevinsaju@sugarlabs.org>
2021-07-01 11:51:24 +0300

    feat: use gradients for album art

--- cmd/desktop/static/js/similarFetcher.js
+++ cmd/desktop/static/js/similarFetcher.js
@@ -1,32 +1,21 @@
 let currentTrack = ""
 let currentArtist = ""
 let currentPlayers = []
+let hexValues = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e"];
 
 
-let RATE_LIMIT_IN_MS = 1000;
-let NUMBER_OF_REQUESTS_ALLOWED = 1;
-let NUMBER_OF_REQUESTS = 0;
-let coverArtQueue = []
 
 
-function getSongHTML(track, artist, albumArt, mbid, artistMbid) {
-
-    let albumArtUrl = "http://bulma.io/images/placeholders/96x96.png"
-    if (albumArt !== "") {
-        albumArtUrl = albumArt
-    }
-
-
+function getSongHTML(track, artist){
     return `<div class="card mb-3">
         <div class="card-content">
             <div class="media">
                 <div class="media-left">
-                    <figure class="image is-48x48">
-                        <img 
+                    <div 
                         data-artist="${artist}"
                         data-track="${track}"
-                        src="${albumArtUrl}" alt="Album art of ${track} by ${artist}">
-                    </figure>
+                        class="image is-48x48 album-art-square">
+                    </div>
                 </div>
                 <div class="media-content">
                     <p class="title is-4">${track}</p>
@@ -77,37 +66,13 @@ $(document).ready(function() {
                 if (item["track"] === "") {
                     return
                 }
-                similarContainer.append(getSongHTML(item["track"], item["artist"], item["album_art"], item["mbid"]))
+                similarContainer.append(getSongHTML(item["track"], item["artist"]))
                 console.log(item["track"], item["artist"], item["album_art"], item["mbid"])
 
             })
             data.forEach(function(item) {
-                let track = item["track"]
-                let artistMbid = item["artist_mbid"]
+                setAlbumArtGradient(item["track"])
 
-                if (artistMbid !== "" && artistMbid !== undefined) {
-
-                    coverArtQueue.push(
-                        function () {
-                            const mbrzUrl = `http://musicbrainz.org/ws/2/release?query=${encodeURI(track)}%20AND%20arid:${artistMbid}&fmt=json`
-                            console.log(mbrzUrl)
-
-                            $.get(mbrzUrl, function (data) {
-                                if (data["releases"] === undefined || data["releases"].length === 0) {
-                                    return
-                                }
-                                const mbId = data["releases"][0]["id"]
-                                $.get(`http://coverartarchive.org/release/${mbId}`, function (data) {
-                                    let albumArtImage = data["images"][0]["image"]
-                                    console.log(albumArtImage)
-                                    $(`[data-track="${track}"]`).attr("src", albumArtImage)
-                                }, "json")
-
-                            }, "json")
-                        }
-                    )
-
-                }
             })
             // $(".lyrics").html(data.replaceAll("\n", "<br>"))
         }, "json")
@@ -156,19 +121,25 @@ $(document).ready(function() {
 });
 
 
-function GetCovertArtRateLimited() {
-    if (coverArtQueue.length === 0) {
-        return
+function setAlbumArtGradient(track, artist) {
+
+
+
+    function populate(a) {
+        for ( let i = 0; i < 6; i++ ) {
+            let x = Math.round( Math.random() * 14 );
+            let y = hexValues[x];
+            a += y;
+        }
+        return a;
     }
-    let fx = coverArtQueue.shift()
 
-    fx()
+    let newColor1 = populate('#');
+    let newColor2 = populate('#');
+    let angle = Math.round( Math.random() * 360 );
+
+    let gradient = "linear-gradient(" + angle + "deg, " + newColor1 + ", " + newColor2 + ")";
+    $(`div[data-track="${track}"]`).css('background', gradient);
+
 }
-
-
-setInterval(function()
-{
-    GetCovertArtRateLimited()
-
-}, RATE_LIMIT_IN_MS);
 
